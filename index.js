bd97ab1d71c757b7f6cf9ec1ec2c2a1ee4195182
Stephen Duffy <duffysd@sunypoly.edu>
2025-11-18 11:39:46 -0500

    initial database setup

--- index.js
+++ index.js
@@ -105,7 +105,10 @@ fastify.post("/postMessage", async function(request,reply){
 
 // Run the server!
 try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({
+            port: 3000,
+            host: "0.0.0.0"
+        })
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
